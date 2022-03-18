b40afa80ed986e3d0fa920a42d39f31e68912c18
Frank Lavery <frank.lavery@westnet.com.au>
2022-03-18 23:34:43 +0800

    plan approach

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+// Make sure to use some JQuery to demonstrate that can
+
+
+// HTML with Header containing title Work Day Scheduler, with sub phrasing and current date in format Thursday, Sept 5th
+// When planner opened, current day is displayed at top of calendar (Use Moment to display)
+
+
+
+// page broken into time blocks of one hour from 9am to 5pm.  Do as list items generated in JS.
+// one hour time blocks, with time on left, wide line for text and button on right with disk symbol for saving an event
+
+
+// can click on timeblock and enter or edit an event.  Click on disk symbol to save.  Save to Local storage
+
+
+// past colours grey, present dark green, future in light green.  Text in black, except when changing, then it is blue
+
+
+// when page loads, copy events from local storage and display on page
+
+
